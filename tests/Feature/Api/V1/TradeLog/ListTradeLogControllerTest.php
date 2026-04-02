cdf6520c5c50ca4e51eb84e0780738f536d5df73
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2026-04-02 22:17:39 +0800

    test: add search, sort, filter, and pagination tests on trade log list api controller

--- tests/Feature/Api/V1/TradeLog/ListTradeLogControllerTest.php
+++ tests/Feature/Api/V1/TradeLog/ListTradeLogControllerTest.php
@@ -6,26 +6,171 @@ use Laravel\Sanctum\Sanctum;
 
 describe('Feature: ListTradeLogController', function () {
 
-    it('should return collection of trade logs resource when using /api/v1/trade-logs GET api endpoint.', function () {
+    describe('Positives', function () {
 
-        // Arrange:
-        $no_of_trade_logs = 15;
-        $portfolio = PortfolioModel::factory()->create();
+        it('should return collection of trade logs resource when using /api/v1/trade-logs GET api endpoint.', function () {
 
-        TradeLogModel::factory($no_of_trade_logs)->for($portfolio)->create();
+            // Arrange:
+            $no_of_trade_logs = 15;
+            $portfolio = PortfolioModel::factory()->create();
 
-        Sanctum::actingAs($portfolio->user);
+            TradeLogModel::factory($no_of_trade_logs)->for($portfolio)->create();
 
-        // Act:
-        $response = $this->get('/api/v1/trade-logs');
+            Sanctum::actingAs($portfolio->user);
 
-        dd($response->json());
+            // Act:
+            $response = $this->get('/api/v1/trade-logs');
 
-        // Assert:
-        $response->assertOk()
-            ->assertJsonPath('success', true)
-            ->assertJsonPath('pagination.total', $no_of_trade_logs);
+            // Assert:
+            $response->assertOk()
+                ->assertJsonPath('success', true)
+                ->assertJsonPath('pagination.total', $no_of_trade_logs);
 
+        });
+
+        it('should paginate trade logs when using /api/v1/trade-logs GET api endpoint.', function () {
+
+            // Arrange:
+            $no_of_trade_logs = 50;
+            $portfolio = PortfolioModel::factory()->create();
+            TradeLogModel::factory()->for($portfolio)->count($no_of_trade_logs)->create();
+
+            $page_number = 3;
+            $per_page = 15;
+
+            $query = http_build_query([
+                'page' => $page_number,
+                'per_page' => $per_page,
+            ]);
+
+            Sanctum::actingAs($portfolio->user);
+
+            // Act:
+            $response = $this->get(sprintf('/api/v1/trade-logs?%s', $query));
+
+            // Assert:
+            $response->assertOk()
+                ->assertJsonPath('success', true)
+                ->assertJsonPath('pagination.current_page', $page_number)
+                ->assertJsonCount($per_page, 'data');
+
+        });
+
+        it('should filter trade logs by type when using /api/v1/trade-logs GET api endpoint.', function () {
+
+            // Arrange:
+            $portfolio = PortfolioModel::factory()->create();
+
+            TradeLogModel::factory()->for($portfolio)->create(['type' => 'buy']);
+            TradeLogModel::factory()->for($portfolio)->create(['type' => 'sell']);
+
+            $query = http_build_query([
+                'filters' => [
+                    ['field' => 'type', 'operator' => '=', 'value' => 'buy'],
+                ],
+            ]);
+
+            Sanctum::actingAs($portfolio->user);
+
+            // Act:
+            $response = $this->get(sprintf('/api/v1/trade-logs?%s', $query));
+
+            // Assert:
+            $response->assertOk()
+                ->assertJsonCount(1, 'data')
+                ->assertJsonPath('data.0.type', 'buy');
+
+        });
+
+        it('should sort trade logs by shares descending when using /api/v1/trade-logs GET api endpoint.', function () {
+
+            // Arrange:
+            $portfolio = PortfolioModel::factory()->create();
+
+            TradeLogModel::factory()->for($portfolio)->create(['shares' => 3500]);
+            TradeLogModel::factory()->for($portfolio)->create(['shares' => 18000]);
+            TradeLogModel::factory()->for($portfolio)->create(['shares' => 290]);
+
+            $query = http_build_query([
+                'sorts' => [
+                    ['field' => 'shares', 'direction' => 'desc'],
+                ],
+            ]);
+
+            Sanctum::actingAs($portfolio->user);
+
+            // Act:
+            $response = $this->get(sprintf('/api/v1/trade-logs?%s', $query));
+
+            $data = $response->json('data');
+
+            $shares = collect($data)->map(fn ($item) => $item['shares'])->all();
+
+            // Assert:
+            expect($shares)->toBe([18000, 3500, 290]);
+
+        });
+
+        it('should search trade logs by symbol when using /api/v1/cashflows GET api endpoint.', function () {
+
+            // Arrange:
+            $portfolio = PortfolioModel::factory()->create();
+
+            $symbol_to_search = 'BPI';
+            TradeLogModel::factory()->for($portfolio)->create(['symbol' => 'AC']);
+            TradeLogModel::factory()->for($portfolio)->create(['symbol' => 'JGS']);
+            TradeLogModel::factory()->for($portfolio)->create(['symbol' => $symbol_to_search]);
+            TradeLogModel::factory()->for($portfolio)->create(['symbol' => 'BDO']);
+
+            $query = http_build_query([
+                'search' => $symbol_to_search,
+            ]);
+
+            Sanctum::actingAs($portfolio->user);
+
+            // Act:
+            $response = $this->get(sprintf('/api/v1/trade-logs?%s', $query));
+
+            // Assert:
+            $response->assertOk()
+                ->assertJsonCount(1, 'data')
+                ->assertJsonPath('data.0.symbol', $symbol_to_search);
+
+        });
+
+        it('should apply search, filter, sort, and pagination together when using /api/v1/trade-logs GET api endpoint.', function () {
+
+            // Arrange:
+            $portfolio = PortfolioModel::factory()->create();
+            $symbol_to_search = 'BPI';
+            TradeLogModel::factory()->for($portfolio)->create(['type' => 'buy', 'symbol' => 'AC', 'shares' => 3500]);
+            TradeLogModel::factory()->for($portfolio)->create(['type' => 'sell', 'symbol' => 'JGS', 'shares' => 18000]);
+            TradeLogModel::factory()->for($portfolio)->create(['type' => 'buy', 'symbol' => $symbol_to_search, 'shares' => 290]);
+
+            $query = http_build_query([
+                'seach' => $symbol_to_search,
+                'page' => 1,
+                'per_page' => 1,
+                'filters' => [
+                    ['field' => 'type', 'operator' => '=', 'value' => 'buy'],
+                ],
+                'sorts' => [
+                    ['field' => 'shares', 'direction' => 'asc'],
+                ],
+            ]);
+
+            Sanctum::actingAs($portfolio->user);
+
+            // Act:
+            $response = $this->get(sprintf('/api/v1/trade-logs?%s', $query));
+
+            // Assert
+            $response->assertOk()
+                ->assertJsonCount(1, 'data')
+                ->assertJsonPath('data.0.type', 'buy')
+                ->assertJsonPath('data.0.shares', 290);
+
+        });
     });
 
 });
--- tests/Integration/Infrastructure/Persistence/Eloquent/Read/EloquentTradeLogReadRepositoryTest.php
+++ tests/Integration/Infrastructure/Persistence/Eloquent/Read/EloquentTradeLogReadRepositoryTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Domain\Common\Query\Filter;
 use App\Domain\Common\Query\QueryCriteria;
+use App\Domain\Common\Query\Sort;
+use App\Enums\OperatorType;
 use App\Infrastructure\Persistence\Eloquent\Read\EloquentTradeLogReadRepository;
 use App\Models\Portfolio as PortfolioModel;
 use App\Models\TradeLog as TradeLogModel;
@@ -11,21 +14,152 @@ beforeEach(function () {
 
 describe('Integration: EloquentTradeLogReadRepository', function () {
 
-    it('should return all trade logs when using findAll method.', function () {
+    describe('Positives', function () {
 
-        // Arrange:
-        $no_of_trade_logs = 10;
-        $portfolio = PortfolioModel::factory()->create();
-        TradeLogModel::factory()->for($portfolio)->count($no_of_trade_logs)->create();
+        it('should return all trade logs when using findAll method.', function () {
 
-        // Act:
-        $result = $this->repository->findAll(new QueryCriteria);
+            // Arrange:
+            $no_of_trade_logs = 10;
+            $portfolio = PortfolioModel::factory()->create();
+            TradeLogModel::factory()->for($portfolio)->count($no_of_trade_logs)->create();
 
-        // Assert:
-        expect($result)
-            ->toBeArray()
-            ->and(expect($result['data'])->toHaveCount($no_of_trade_logs));
+            // Act:
+            $result = $this->repository->findAll(new QueryCriteria);
 
+            // Assert:
+            expect($result)
+                ->toBeArray()
+                ->and(expect($result['data'])->toHaveCount($no_of_trade_logs));
+
+        });
+
+        it('should paginate correctly when using findAll method.', function () {
+
+            // Arrange:
+            $no_of_trade_logs = 50;
+            TradeLogModel::factory($no_of_trade_logs)->create();
+
+            $page_number = 2;
+            $per_page = 10;
+
+            $criteria = new QueryCriteria(
+                page: $page_number,
+                per_page: $per_page,
+            );
+
+            // Act:
+            $result = $this->repository->findAll($criteria);
+
+            // Assert:
+            expect($result)
+                ->toBeArray()
+                ->and(expect($result['pagination']['current_page'])->toBe($page_number))
+                ->and(count($result['data']))->toBe($per_page);
+
+        });
+
+        it('should filter by type when using findAll method.', function () {
+
+            // Arrange:
+            TradeLogModel::factory()->create(['type' => 'buy']);
+            TradeLogModel::factory()->create(['type' => 'sell']);
+
+            $criteria = new QueryCriteria(
+                filters: [
+                    new Filter('type', OperatorType::EQ->value, 'buy'),
+                ]
+            );
+
+            // Act:
+            $result = $this->repository->findAll($criteria);
+
+            // Assert:
+            expect($result['data'])
+                ->toHaveCount(1)
+                ->and($result['data'][0]->type())
+                ->toBe('buy');
+
+        });
+
+        it('should sort by shares descending when using findAll method.', function () {
+
+            // Arrange:
+            TradeLogModel::factory()->create(['shares' => 3500]);
+            TradeLogModel::factory()->create(['shares' => 18000]);
+            TradeLogModel::factory()->create(['shares' => 290]);
+
+            $criteria = new QueryCriteria(
+                sorts: [
+                    new Sort('shares', 'desc'),
+                ]
+            );
+
+            // Act:
+            $result = $this->repository->findAll($criteria);
+
+            $shares = collect($result['data'])->map(fn ($item) => $item->shares())->all();
+
+            // Assert
+            expect($shares)->toBe([18000, 3500, 290]);
+
+        });
+
+        it('should search by symbol when using findAll method.', function () {
+
+            // Arrange:
+            $symbol_to_search = 'BPI';
+            TradeLogModel::factory()->create(['symbol' => 'AC']);
+            TradeLogModel::factory()->create(['symbol' => 'JGS']);
+            TradeLogModel::factory()->create(['symbol' => $symbol_to_search]);
+            TradeLogModel::factory()->create(['symbol' => 'BDO']);
+
+            $criteria = new QueryCriteria(
+                search: $symbol_to_search
+            );
+
+            // Act
+            $result = $this->repository->findAll($criteria);
+
+            // Assert
+            expect($result['data'])
+                ->toHaveCount(1)
+                ->and($result['data'][0]->symbol())
+                ->toBe($symbol_to_search);
+
+        });
+
+        it('should apply search, filter, sort, and pagination together when using findAll method.', function () {
+
+            // Arrange:
+            $symbol_to_search = 'BPI';
+            TradeLogModel::factory()->create(['type' => 'buy', 'symbol' => 'AC', 'shares' => 3500]);
+            TradeLogModel::factory()->create(['type' => 'sell', 'symbol' => 'JGS', 'shares' => 18000]);
+            TradeLogModel::factory()->create(['type' => 'buy', 'symbol' => $symbol_to_search, 'shares' => 290]);
+
+            $criteria = new QueryCriteria(
+                page: 1,
+                per_page: 1,
+                search: $symbol_to_search,
+                filters: [
+                    new Filter('type', OperatorType::EQ->value, 'buy'),
+                ],
+                sorts: [
+                    new Sort('shares', 'asc'),
+                ]
+            );
+
+            // Act:
+            $result = $this->repository->findAll($criteria);
+
+            // Assert
+            expect($result['data'])
+                ->toHaveCount(1)
+                ->and($result['data'][0]->type())
+                ->toBe('buy')
+                ->and($result['data'][0]->shares())
+                ->toBe(290);
+
+        });
     });
 
 });
