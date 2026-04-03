3e564816fffff2ecdc3ae2dd747c9d3802d3c5f5
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2026-04-03 19:53:02 +0800

    test: add negative scenario on feature test for trade log store and update api endpoint

--- tests/Feature/Api/V1/TradeLog/StoreTradeLogControllerTest.php
+++ tests/Feature/Api/V1/TradeLog/StoreTradeLogControllerTest.php
@@ -1,43 +1,87 @@
 <?php
 
+use App\Application\TradeLog\UseCases\StoreTradeLog;
 use App\Models\Portfolio as PortfolioModel;
 use Laravel\Sanctum\Sanctum;
+use Mockery\MockInterface;
 
 describe('Feature: StoreTradeLogController', function () {
 
-    it('should store new trade log resource when using /api/v1/trade-logs POST api endpoint.', function () {
+    describe('Positives', function () {
 
-        // Arrange:
-        $portfolio = PortfolioModel::factory()->create();
+        it('should store new trade log resource when using /api/v1/trade-logs POST api endpoint.', function () {
 
-        $payload = [
-            'portfolio_id' => $portfolio->id,
-            'symbol' => 'BPI',
-            'type' => 'buy',
-            'price' => 100,
-            'shares' => 1000,
-            'fees' => 120,
-        ];
+            // Arrange:
+            $portfolio = PortfolioModel::factory()->create();
 
-        // Act:
-        Sanctum::actingAs($portfolio->user);
-        $response = $this->post('/api/v1/trade-logs', $payload);
+            $payload = [
+                'portfolio_id' => $portfolio->id,
+                'symbol' => 'BPI',
+                'type' => 'buy',
+                'price' => 100,
+                'shares' => 1000,
+                'fees' => 120,
+            ];
 
-        // Assert:
-        $response->assertCreated()
-            ->assertJson([
-                'success' => true,
-                'data' => [
-                    'symbol' => $payload['symbol'],
-                    'type' => $payload['type'],
-                    'price' => $payload['price'],
-                    'shares' => $payload['shares'],
-                    'fees' => $payload['fees'],
-                    'portfolio' => [
-                        'id' => $payload['portfolio_id'],
+            // Act:
+            Sanctum::actingAs($portfolio->user);
+            $response = $this->post('/api/v1/trade-logs', $payload);
+
+            // Assert:
+            $response->assertCreated()
+                ->assertJson([
+                    'success' => true,
+                    'data' => [
+                        'symbol' => $payload['symbol'],
+                        'type' => $payload['type'],
+                        'price' => $payload['price'],
+                        'shares' => $payload['shares'],
+                        'fees' => $payload['fees'],
+                        'portfolio' => [
+                            'id' => $payload['portfolio_id'],
+                        ],
                     ],
-                ],
-            ]);
+                ]);
+        });
+
+    });
+
+    describe('Negatives', function () {
+
+        it('should handle server error response when using /api/v1/trade-logs POST api endpoint.', function () {
+
+            // Arrange:
+            $portfolio = PortfolioModel::factory()->create();
+
+            $payload = [
+                'portfolio_id' => $portfolio->id,
+                'symbol' => 'BPI',
+                'type' => 'buy',
+                'price' => 100,
+                'shares' => 1000,
+                'fees' => 120,
+            ];
+
+            // Expectation:
+            $this->mock(StoreTradeLog::class, function (MockInterface $mock) {
+                $mock->shouldReceive('handle')
+                    ->once()
+                    ->andThrow(new Exception('This is a mock exception message.'));
+            });
+
+            // Act:
+            Sanctum::actingAs($portfolio->user);
+            $response = $this->post('/api/v1/trade-logs', $payload);
+
+            // Assert:
+            $response->assertInternalServerError()
+                ->assertJson([
+                    'success' => false,
+                    'error' => 'An unexpected error occurred. Please try again later.',
+                    'message' => 'This is a mock exception message.',
+                ]);
+        });
+
     });
 
 });
--- tests/Feature/Api/V1/TradeLog/UpdateTradeLogControllerTest.php
+++ tests/Feature/Api/V1/TradeLog/UpdateTradeLogControllerTest.php
@@ -1,38 +1,83 @@
 <?php
 
+use App\Application\TradeLog\UseCases\StoreTradeLog;
 use App\Models\TradeLog as TradeLogModel;
 use Laravel\Sanctum\Sanctum;
+use Mockery\MockInterface;
 
 describe('Feature: UpdateTradeLogController', function () {
 
-    it('should update existing trade log resource when using /api/v1/trade-logs PUT api endpoint.', function () {
+    describe('Positives', function () {
 
-        // Arrange:
-        $trade_log = TradeLogModel::factory()->create();
+        it('should update existing trade log resource when using /api/v1/trade-logs PUT api endpoint.', function () {
 
-        $payload = [
-            'portfolio_id' => $trade_log->portfolio->id,
-            'symbol' => $trade_log->symbol,
-            'type' => $trade_log->type,
-            'price' => 110,
-            'shares' => $trade_log->shares,
-            'fees' => $trade_log->fees,
-        ];
+            // Arrange:
+            $trade_log = TradeLogModel::factory()->create();
 
-        // Act:
-        Sanctum::actingAs($trade_log->portfolio->user);
-        $response = $this->put('/api/v1/trade-logs', $payload);
+            $payload = [
+                'portfolio_id' => $trade_log->portfolio->id,
+                'symbol' => $trade_log->symbol,
+                'type' => $trade_log->type,
+                'price' => 110,
+                'shares' => $trade_log->shares,
+                'fees' => $trade_log->fees,
+            ];
 
-        // Assert:
-        $this->assertDatabaseHas('trade_logs', $payload);
+            // Act:
+            Sanctum::actingAs($trade_log->portfolio->user);
+            $response = $this->put('/api/v1/trade-logs', $payload);
 
-        $response->assertOk()
-            ->assertJson([
-                'success' => true,
-                'data' => [
-                    'price' => $payload['price'],
-                ],
-            ]);
+            // Assert:
+            $this->assertDatabaseHas('trade_logs', $payload);
+
+            $response->assertOk()
+                ->assertJson([
+                    'success' => true,
+                    'data' => [
+                        'price' => $payload['price'],
+                    ],
+                ]);
+        });
+
+    });
+
+    describe('Negatives', function () {
+
+        it('should handle server error response when using /api/v1/trade-logs PUT api endpoint.', function () {
+
+            // Arrange:
+            $trade_log = TradeLogModel::factory()->create();
+
+            $payload = [
+                'portfolio_id' => $trade_log->portfolio->id,
+                'symbol' => $trade_log->symbol,
+                'type' => $trade_log->type,
+                'price' => 110,
+                'shares' => $trade_log->shares,
+                'fees' => $trade_log->fees,
+            ];
+
+            // Expectation:
+            $this->mock(StoreTradeLog::class, function (MockInterface $mock) {
+                $mock->shouldReceive('handle')
+                    ->once()
+                    ->andThrow(new Exception('This is a mock exception message.'));
+            });
+
+            // Act:
+            Sanctum::actingAs($trade_log->portfolio->user);
+            $response = $this->put('/api/v1/trade-logs', $payload);
+
+            // Assert:
+            $response->assertInternalServerError()
+                ->assertJson([
+                    'success' => false,
+                    'error' => 'An unexpected error occurred. Please try again later.',
+                    'message' => 'This is a mock exception message.',
+                ]);
+
+        });
+
     });
 
 });
