546080968f7eeb1e355d15e0a80325d2799528d3
Carl Anthony Mabugay <mabugay.carl@gmail.com>
2026-04-19 22:41:18 +0800

    chore: setup shadcn-vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,10 +1,36 @@
 <script setup>
 
+import { Button } from '@/components/ui/button/index.ts'
+import { Input } from '@/components/ui/input/index.ts'
+import {
+    Card,
+    CardTitle,
+    CardHeader,
+    CardDescription,
+    CardContent,
+    CardFooter,
+    CardAction
+} from '@/components/ui/card/index.ts'
 </script>
 
 <template>
-    <div class="min-h-screen  bg-black">
-        <h1 class="text-3xl text-lime-300">Hello, Mom!</h1>
+    <div class="min-h-screen max-w-7xl mx-auto my-6">
+        <Card>
+            <CardHeader>
+                <CardTitle>Basic Template</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Input type="text" placeholder="Hi, Mom!"/>
+            </CardContent>
+            <CardFooter>
+                <CardAction>
+                    <Button>Click me</Button>
+                </CardAction>
+            </CardFooter>
+        </Card>
+
+
     </div>
 
 </template>
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,11 @@ export default defineConfig({
         tailwindcss(),
         vue(),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
     server: {
         host: '0.0.0.0',
         port: 5173,
